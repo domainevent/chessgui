6d524815d8b12ed413c24c63d8d42cc2d3a6eceb
javacook <javacook@gmx.de>
2017-05-03 07:42:57 +0200

    initial

---
+++ src/main/java/com/javacook/chessgui/MoveException.java
@@ -0,0 +1,11 @@
+package com.javacook.chessgui;
+
+/**
+ * Created by vollmer on 02.05.17.
+ */
+public class MoveException extends Exception {
+
+    public MoveException(String message) {
+        super(message);
+    }
+}
