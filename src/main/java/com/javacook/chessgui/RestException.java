a211647ff5717eef5c87263abb4d030ccd147dcc
javacook <javacook@gmx.de>
2017-05-04 07:08:13 +0200

    initial

---
+++ src/main/java/com/javacook/chessgui/RestException.java
@@ -0,0 +1,14 @@
+package com.javacook.chessgui;
+
+/**
+ * Created by vollmer on 02.05.17.
+ */
+public class RestException extends Exception {
+
+    public RestException() {
+    }
+
+    public RestException(String message) {
+        super(message);
+    }
+}
