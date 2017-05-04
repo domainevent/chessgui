5d528200d2660ed7b3666e3996be4ed0deec568b
javacook <javacook@gmx.de>
2017-05-04 07:12:50 +0200

    extends now RestException

--- src/main/java/com/javacook/chessgui/MoveException.java
+++ src/main/java/com/javacook/chessgui/MoveException.java
@@ -1,9 +1,12 @@
 package com.javacook.chessgui;
 
+import com.sun.org.apache.regexp.internal.RE;
+
+
 /**
  * Created by vollmer on 02.05.17.
  */
-public class MoveException extends Exception {
+public class MoveException extends RestException {
 
     public MoveException(String message) {
         super(message);
