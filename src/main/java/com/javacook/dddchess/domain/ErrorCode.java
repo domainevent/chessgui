e577e3a704e63bc88f4c46ef9b7d39bd026a9b00
javacook <javacook@gmx.de>
2017-05-04 07:07:38 +0200

    initial

---
+++ src/main/java/com/javacook/dddchess/domain/ErrorCode.java
@@ -0,0 +1,13 @@
+package com.javacook.dddchess.domain;
+
+
+/**
+ * Created by vollmer on 02.05.17.
+ */
+public enum ErrorCode {
+
+    TIMEOUT,
+    INVALID_MOVE;
+
+    public final static String ERROR_CODE_KEY = "error code";
+}
