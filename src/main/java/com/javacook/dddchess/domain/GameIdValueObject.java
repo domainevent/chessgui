4837a3f7d47c0c7f29dc6acb8a3d168ba619eb6f
javacook <javacook@gmx.de>
2017-05-07 23:31:35 +0200

    initial

---
+++ src/main/java/com/javacook/dddchess/domain/GameIdValueObject.java
@@ -0,0 +1,18 @@
+package com.javacook.dddchess.domain;
+
+import java.util.UUID;
+
+
+/**
+ * Created by vollmer on 05.05.17.
+ */
+public class GameIdValueObject {
+
+    public final String id = UUID.randomUUID().toString();
+
+    @Override
+    public String toString() {
+        return id;
+    }
+
+}
