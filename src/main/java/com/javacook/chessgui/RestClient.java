d5cacb3838308c4c04a5752ba03ce10085bf691d
javacook <javacook@gmx.de>
2017-05-07 14:36:30 +0200

    initial

---
+++ src/main/java/com/javacook/chessgui/RestClient.java
@@ -0,0 +1,19 @@
+package com.javacook.chessgui;
+
+import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+
+
+/**
+ * Created by vollmer on 07.05.17.
+ */
+public class RestClient {
+
+    public final static String SERVER_URL = "http://localhost:8080/dddtutorial/chessgames";
+
+    public final static Client CLIENT = ClientBuilder.newClient().register(JacksonJsonProvider.class);
+
+}
+
