640310dec5aa98a38a4ad7fc4e361d765f2143d1
Jörg Vollmer <javacook@gmx.de>
2017-05-13 13:11:11 +0200

    Timeouts definierit

--- src/main/java/com/javacook/chessgui/RestClient.java
+++ src/main/java/com/javacook/chessgui/RestClient.java
@@ -1,6 +1,7 @@
 package com.javacook.chessgui;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
+import org.glassfish.jersey.client.ClientProperties;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
@@ -13,10 +14,15 @@ public class RestClient {
 
 //    public final static String DEFAULT_SERVER_URL = "http://localhost:8080/dddchess/api";
     public final static String DEFAULT_SERVER_URL = "http://localhost:8080/dddschach/api";
+    public final static int TIMEOUT = 2000;
 
     public static String SERVER_URL = DEFAULT_SERVER_URL;
 
-    public final static Client CLIENT = ClientBuilder.newClient().register(JacksonJsonProvider.class);
+    public final static Client CLIENT = ClientBuilder
+            .newClient()
+            .register(JacksonJsonProvider.class)
+            .property(ClientProperties.CONNECT_TIMEOUT, TIMEOUT)
+            .property(ClientProperties.READ_TIMEOUT,    TIMEOUT);
 
 }
 
